31e8c784b95e29a5eaacd3ec04e8f1b07a394883
Hannah Coombe <drhjcoombe@gmail.com>
2024-04-18 23:15:11 -0400

    Added comment

--- Amp.py
+++ Amp.py
@@ -1,4 +1,11 @@
 def calculate_dose_mg(kg:float):
+    """Calculates Ampicillin dose for infants
+
+    Args:
+        kg (float): Baby's weight in Kg's
+    Returns:
+       str: Dose note statement containing dose in mg
+    """
     return f"{round(100*kg)}mg IV Ampicillin TID"
 
 
